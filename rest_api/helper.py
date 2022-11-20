a64994fc01295e545437234fa47eb8d87a8c9c31
sajmon <sajmon>
2022-11-20 22:36:08 +0100

    added slow down decorator

--- rest_api/helper.py
+++ rest_api/helper.py
@@ -1,4 +1,7 @@
-from typing import Dict
+from random import randrange
+from functools import wraps
+from time import sleep
+from typing import Dict, Tuple
 
 from pydantic import BaseModel
 
@@ -11,3 +14,19 @@ def get_api_data(data_generator: DataGenerator) -> Dict[int, BaseModel]:
     :return: dict with *int* id as key and *BaseModel* as value
     """
     return data_generator.generate_data()
+
+
+def random_slow_down(rand_range: Tuple[int, int]):
+    """
+    Wait a random amount of milliseconds before the execution of a decorated function
+    :param rand_range: *tuple* range from which a random value is chosen
+    :return:
+    """
+    def inner(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            time_to_wait = randrange(*rand_range) / 1000
+            sleep(time_to_wait)
+            return func(*args, **kwargs)
+        return wrapper
+    return inner
